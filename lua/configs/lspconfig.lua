e9efc484e0202487603ab7318407340033ef5b87
Mateusz Kurzyna <lolc44t@gmail.com>
2025-05-27 20:15:57 +0200

    Add trouble.nvim and adjust LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,3 +3,33 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
+vim.diagnostic.config({
+  virtual_text = { severity = { min = vim.diagnostic.severity.WARN } },
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
+})
+
+-- Format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
+
+-- Auto-enable inlay hints for all LSP-attached buffers
+local inlay_hint_group = vim.api.nvim_create_augroup("LspInlayHints", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = inlay_hint_group,
+  callback = function(args)
+    if vim.lsp.inlay_hint then
+      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    end
+  end,
+})
+
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,9 +1,6 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    -- either:
-    event = "VimEnter",  -- load on VimEnter
-    -- or, if you want it *always* loaded at startup:
-    -- lazy = false,
+    lazy = false
   },
 }
