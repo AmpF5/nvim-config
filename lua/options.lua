4419b0ecd6e4d17a36b6a87dad140fbf82d7ae7a
Mateusz Kurzyna <lolc44t@gmail.com>
2025-05-28 21:42:59 +0200

    init lazyvim

--- lua/options.lua
+++
@@ -1,8 +0,0 @@
-require "nvchad.options"
-
-
--- line-number style ------------------------------------------------------------
-vim.opt.number         = true   -- show the absolute number on the current line
-vim.opt.relativenumber = true   -- ...and relative numbers everywhere else
-
-
