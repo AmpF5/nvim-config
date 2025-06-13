627a3dcfb0d29af76dd11a0f346b4519d2f75161
Mateusz Kurzyna <lolc44t@gmail.com>
2025-06-13 19:02:31 +0200

    fix typo, add new plugins

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -1,12 +1,10 @@
 -- Golang plugin configuration for Neovim
 return {
-  -- Install Go development plugins
   {
     "fatih/vim-go",
     ft = { "go" },
     build = ":GoUpdateBinaries",
     config = function()
-      -- Optional: Enable goimports on save
       vim.g.go_fmt_command = "goimports"
     end,
   },
@@ -16,13 +14,14 @@ return {
     ft = { "go", "gomod" },
     config = function()
       require("go").setup({
-        lsp_cfg = true, -- use go.nvim's default LSP config
+        lsp_cfg = true,
         lsp_inlay_hints = {
           enable = true,
         },
         lsp_keymaps = true,
         lsp_codelens = true,
         dap_debug = true,
-      })z
+      })
+    end,
   },
 }
